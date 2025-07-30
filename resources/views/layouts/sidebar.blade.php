e00123a89f3553187ff2b61c68c6ca20cb38b3a1
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2025-07-30 19:07:43 +0700

    Add layout files and dashboard view for inventory management
    - Created header, sidebar, and main layout files for consistent UI.
    - Implemented dashboard view with widgets displaying stock and user statistics.
    - Updated routes to include dashboard access.

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,46 @@
+ <aside id="left-panel" class="left-panel">
+     <nav class="navbar navbar-expand-sm navbar-default">
+         <div id="main-menu" class="main-menu collapse navbar-collapse">
+             <ul class="nav navbar-nav">
+                 <li class="active">
+                     <a href="index.html"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                 </li>
+                 <li class="menu-title">Master</li><!-- /.menu-title -->
+                 <li>
+                     <a href="widgets.html"> <i class="menu-icon fa fa-cogs"></i>Category</a>
+                 </li>
+                 <li>
+                     <a href="widgets.html"> <i class="menu-icon fa fa-table"></i>Supplier</a>
+                 </li>
+                 <li>
+                     <a href="widgets.html"> <i class="menu-icon fa fa-bar-chart-o"></i>EOQ Setting</a>
+                 </li>
+                 <li>
+                     <a href="widgets.html"> <i class="menu-icon fa fa-users"></i>User Management</a>
+                 </li>
+
+
+                 <li class="menu-title">Menu</li><!-- /.menu-title -->
+
+
+                 <li>
+                     <a href="widgets.html"> <i class="menu-icon fa fa-tags"></i>Product</a>
+                 </li>
+                 <li>
+                     <a href="widgets.html"> <i class="menu-icon fa fa-arrow-down"></i>Stock In</a>
+                 </li>
+                 <li>
+                     <a href="widgets.html"> <i class="menu-icon fa fa-arrow-up"></i>Stock Out</a>
+                 </li>
+
+                 <li class="menu-title">Reports</li><!-- /.menu-title -->
+                 <li>
+                     <a href="widgets.html"> <i class="menu-icon fa fa-cloud-download"></i>Report In</a>
+                 </li>
+                 <li>
+                     <a href="widgets.html"> <i class="menu-icon fa fa-cloud-upload"></i>Report Out</a>
+                 </li>
+             </ul>
+         </div><!-- /.navbar-collapse -->
+     </nav>
+ </aside>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.main');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
